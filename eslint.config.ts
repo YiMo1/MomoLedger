0453cfb47b976b68f1d8f25fb38691af8e4ee534
伊墨 <1905672206@qq.com>
2025-03-12 21:23:44 +0800

    style: configure flow code parser for import-x

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,6 @@
 import { yimo } from 'eslint-config-yimo'
 
-export default yimo()
+export default yimo({}, {
+  rules: { 'import-x/named': 'off' }, // TODO: in next version remove
+  settings: { 'import-x/parsers': { 'hermes-eslint': ['.js'] } },
+})
