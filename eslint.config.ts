f978559deb7d73f13d78d06aff42fe77c6899c2c
伊墨 <1905672206@qq.com>
2025-03-14 19:37:52 +0800

    deps: update eslint-config-yimo to 1.0.5

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,5 @@
 import { yimo } from 'eslint-config-yimo'
 
 export default yimo({}, {
-  rules: { 'import-x/named': 'off' }, // TODO: in next version remove
   settings: { 'import-x/parsers': { 'hermes-eslint': ['.js'] } },
 })
