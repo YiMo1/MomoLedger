cb864b6c37a4bb255b1d621cdd7bf7f844ef017e
伊墨 <1905672206@qq.com>
2025-03-16 20:58:39 +0800

    style: add `stylistic/jsx-self-closing-comp`

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,4 +2,12 @@ import { yimo } from 'eslint-config-yimo'
 
 export default yimo({}, {
   settings: { 'import-x/parsers': { 'hermes-eslint': ['.js'] } },
+  rules: {
+    'stylistic/jsx-self-closing-comp': [ // next version remove
+      'error', {
+        component: true,
+        html: true,
+      },
+    ],
+  },
 })
