fdfe8af85a32bbf835affdb562ebad8b53c350b5
伊墨 <1905672206@qq.com>
2025-03-19 18:55:40 +0800

    deps: update eslint-config-yimo to ^1.0.6

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,14 +2,4 @@ import { yimo } from 'eslint-config-yimo'
 
 export default yimo({}, {
   settings: { 'import-x/parsers': { 'hermes-eslint': ['.js'] } },
-  rules: {
-    'stylistic/jsx-self-closing-comp': [ // next version remove
-      'error', {
-        component: true,
-        html: true,
-      },
-    ],
-    // next version remove
-    'stylistic/jsx-one-expression-per-line': ['error', { allow: 'single-line' }],
-  },
 })
